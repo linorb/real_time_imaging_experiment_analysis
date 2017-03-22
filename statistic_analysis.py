197e2ca4d91c060264fc94dc34c7621ddd8e8c93
Linor BT <astrolinor@gmail.com>
2017-03-22 11:37:59 +0200

    Change the analysis for P value per session (concatination of all trial's events to single session events matrix) instead of per trial

--- statistic_analysis.py
+++ statistic_analysis.py
@@ -25,6 +25,10 @@ CHOSEN_CELLS = np.array([36, 53, 80, 89, 158, 181, 195, 229, 258,
 #                          448, 460, 472, 473, 474, 479, 488,
 #                          501, 517, 569])
 
+
+NUMBER_OF_ACTIVE_ROIS = 2
+NUMBER_OF_PERMUTATIONS = 1000
+
 def create_output_directory(dirname):
     try:
         os.mkdir(dirname)
@@ -115,11 +119,11 @@ def match_path(config_filename):
 
     return results_path
 
-def calculate_p_value_for_one_trial(results_path, chosen_cells):
+def calculate_p_value_for_one_trial(results_path, chosen_cells, number_of_active_rois):
     """Calculate P value for a trial. make the complete procedures,
      including the matching to the right live events"""
     # Loading events and real-time events
-    config_filename = results_path + "\\" + "config.txt"
+    # config_filename = results_path + "\\" + "config.txt"
     events_filename = results_path + "\\" + "events.npy"
     events = np.load(events_filename)
 
@@ -138,8 +142,7 @@ def calculate_p_value_for_one_trial(results_path, chosen_cells):
     not_chosen_events = events[rest_of_cells, :]
 
     number_of_rois = len(chosen_cells)
-    number_of_active_rois = 2
-    number_of_permutations = 1000
+
     number_of_original_activations = count_number_of_activations(live_events,
                                                                  number_of_active_rois)
 
@@ -151,34 +154,52 @@ def calculate_p_value_for_one_trial(results_path, chosen_cells):
 
     return p_value, number_of_activations, number_of_original_activations
 
-def calculate_p_value_for_one_session(session_path, chosen_cells):
-    list_of_dirs = os.listdir(session_path)
-    p_value_entire_session = []
-    number_of_activations_entire_session = []
-    number_of_original_activations = []
-    for dir_name in list_of_dirs:
-        if dir_name[:7] == 'results':
-            results_path = session_path + "\\" + dir_name
-            [p_value, number_of_activations, original_activation] = calculate_p_value_for_one_trial(results_path, chosen_cells)
-            p_value_entire_session.append(p_value)
-            number_of_activations_entire_session.append(number_of_activations)
-            number_of_original_activations.append(original_activation)
+def calculate_p_value_for_one_session(session_path, chosen_cells, number_of_active_rois, number_of_permutations):
+    list_of_trial_dirs = os.listdir(session_path)
 
-    return  p_value_entire_session, number_of_activations_entire_session, number_of_original_activations
+    all_session_events = []
+    for trial_dir_name in list_of_trial_dirs:
+        if trial_dir_name[:7] == 'results':
+            results_path = session_path + "\\" + trial_dir_name
+            trial_events = np.load(results_path + "\\" + "events.npy")
+            all_session_events.append(trial_events)
+
+    # Concatenate all events ot trials to one events mat of the whole session
+    all_session_events = np.concatenate(all_session_events, axis=1)
+
+    chosen_rois_events = all_session_events[chosen_cells, :]
+    number_of_original_activations = count_number_of_activations(chosen_rois_events, number_of_active_rois)
+    number_of_rois = len(chosen_cells)
+
+    number_of_neurons = all_session_events.shape[0]
+
+    # Creating an array that contains the numbers of cells that were not chosen:
+    rest_of_cells = np.arange(number_of_neurons)
+    rest_of_cells[chosen_cells] = 0
+    rest_of_cells = rest_of_cells > 0
+    not_chosen_events = all_session_events[rest_of_cells, :]
+
+    [p_value_entire_session, number_of_activations_all_permutations] = find_p_value_for_activation(not_chosen_events,
+                                                                  number_of_rois,
+                                                                  number_of_active_rois,
+                                                                  number_of_permutations,
+                                                                  number_of_original_activations)
+
+    return  p_value_entire_session, number_of_activations_all_permutations, number_of_original_activations
 
 def main():
     """This main is for a full experiment p value calculation (more then one session)"""
     full_path = RESULTS_PATH
     list_of_sessions = os.listdir(full_path)
     permutations_number_of_activations = []
-    p_value_per_trial = []
+    p_value_per_session = []
     all_session_activations = []
     for dir_name in list_of_sessions:
         if dir_name[:4] == '2017':
             session_path = full_path + "\\" + dir_name + "\\" + 'c' + CAGE + 'm' + MOUSE
             [p_value, number_of_activations, number_of_original_activations] = \
-                calculate_p_value_for_one_session(session_path, CHOSEN_CELLS)
-            p_value_per_trial.append(p_value)
+                calculate_p_value_for_one_session(session_path, CHOSEN_CELLS, NUMBER_OF_ACTIVE_ROIS, NUMBER_OF_PERMUTATIONS)
+            p_value_per_session.append(p_value)
             permutations_number_of_activations.append(number_of_activations)
             all_session_activations.append(number_of_original_activations)
 
@@ -189,12 +210,12 @@ def main():
     create_output_directory(timed_output_dirname)
 
     np.savez(timed_output_dirname + r'\p_values.npz',
-             p_value_per_trial = p_value_per_trial,
+             p_value_per_session = p_value_per_session,
              all_permutatioms_activations = permutations_number_of_activations,
              all_session_activations = all_session_activations)
 
     sio.savemat(timed_output_dirname + r'\p_values.mat',
-                {'p_value_per_trial': p_value_per_trial,
+                {'p_value_per_session': p_value_per_session,
                  'all_permutatioms_activations': permutations_number_of_activations,
                  'all_session_activations': all_session_activations})
 
